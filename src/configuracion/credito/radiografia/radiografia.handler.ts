49c6aadecf7c5794179942e80a4f84ed3a4ceb8e
Daniel De la Torre <marco11.mendia@gmail.com>
2026-01-23 22:10:45 -0600

    radiografias | activity log interceptor implemented

--- src/configuracion/credito/radiografia/radiografia.handler.ts
+++ src/configuracion/credito/radiografia/radiografia.handler.ts
@@ -1,7 +1,12 @@
-import { Controller, Logger } from "@nestjs/common";
+import { Controller, Logger, UseInterceptors } from "@nestjs/common";
 import { EventPattern, MessagePattern, Payload } from "@nestjs/microservices";
 import { RadiografiaService } from "./radiografia.service";
 import { RadioAreaEnum } from "src/configuracion/control-carga-radiografias/enums/control-carga-radio-area.enum";
+import { ActivityLog } from "src/common/decorators/activity-log.decorator";
+import { AuditActionEnum } from "src/common/enums/audit-action.enum";
+import { AuditSourceEnum } from "src/common/enums/audit-source.enum";
+import { ActivityLogRpcInterceptor } from "src/common/interceptor/activity-log-rpc.interceptor";
+import { Usuario } from "src/configuracion/usuarios/entities/usuario.entity";
 
 @Controller()
 export class RadiografiaHandler {
@@ -19,9 +24,18 @@ export class RadiografiaHandler {
         await this._service.parseFileAndBuildCreateRA01CreditoInput(key, cooperativaCodigo);
     }
 
+    @UseInterceptors(ActivityLogRpcInterceptor)
+    @ActivityLog({
+        service: 'config-ms',
+        module: 'credito',
+        action: AuditActionEnum.UPLOAD,
+        source: AuditSourceEnum.JOB,
+        eventName: 'config.radiografias.upload',
+        entities: [],
+    })
     @EventPattern('config.radiografias.upload')
     async handleCrearCargaMasivaRadiografia(
-        @Payload() { key, cooperativaCodigo, area }: { key: string; cooperativaCodigo: string, area: RadioAreaEnum },
+        @Payload() { key, cooperativaCodigo, area }: { key: string; cooperativaCodigo: string, area: RadioAreaEnum, user: Usuario },
     ) {        
         await this._service.executeCarga(key, cooperativaCodigo, area);
     }
