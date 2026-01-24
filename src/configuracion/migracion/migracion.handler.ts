20f2c539c90264215a2ec7d41491df6f6dd26fb0
Daniel De la Torre <marco11.mendia@gmail.com>
2026-01-23 22:06:21 -0600

    migracion | implemented activity log interceptor

--- src/configuracion/migracion/migracion.handler.ts
+++ src/configuracion/migracion/migracion.handler.ts
@@ -1,8 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, UseInterceptors } from "@nestjs/common";
 import { EventPattern, MessagePattern, Payload } from "@nestjs/microservices";
 import { MigracionService } from "./migracion.service";
 import { BooleanResponse } from "src/common/dto/boolean-response.object";
 import { MigracionRequestInput } from "./dto/input/migracion-request.input";
+import { ActivityLog } from "src/common/decorators/activity-log.decorator";
+import { AuditActionEnum } from "src/common/enums/audit-action.enum";
+import { AuditSourceEnum } from "src/common/enums/audit-source.enum";
+import { ActivityLogRpcInterceptor } from "src/common/interceptor/activity-log-rpc.interceptor";
+import { Usuario } from "../usuarios/entities/usuario.entity";
 
 @Controller()
 export class MigracionHandler {
@@ -41,9 +46,18 @@ export class MigracionHandler {
     //     });
     // }
 
+    @UseInterceptors(ActivityLogRpcInterceptor)
+    @ActivityLog({
+        service: 'config-ms',
+        module: 'migracion',
+        action: AuditActionEnum.UPLOAD,
+        source: AuditSourceEnum.JOB,
+        eventName: 'config.migracion.sistema',
+        entities: [],
+    })
     @EventPattern('config.migracion.sistema')
     async handleSisconcreF1(
-        @Payload() { input } : { input: MigracionRequestInput }
+        @Payload() { input } : { input: MigracionRequestInput, user: Usuario }
     ): Promise<BooleanResponse> {
         return await this._service.ejecutarMigracion(input)
     }
--- src/configuracion/migracion/migracion.module.ts
+++ src/configuracion/migracion/migracion.module.ts
@@ -3,9 +3,11 @@ import { MigracionService } from './migracion.service';
 import { MigracionResolver } from './migracion.resolver';
 import { MigracionHandler } from './migracion.handler';
 import { ExcelModule } from 'src/common/excel/excel.module';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   imports: [
+    NatsModule,
     ExcelModule,
   ],
   providers: [MigracionResolver, MigracionService],
